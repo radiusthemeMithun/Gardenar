8018cb449983775a607298e00d3e5a053937e845
radiusthemeMithun <90020665+radiusthemeMithun@users.noreply.github.com>
2026-02-04 11:10:14 +0600

    security working start

---
+++ gardenar.php
@@ -0,0 +1,13 @@
+
+<?php
+/*
+Plugin Name: Gardenar
+Plugin URI: https://www.radiustheme.com
+Description: Gardenar Theme
+Version: 1.1.2
+Author: RadiusTheme
+Author URI: https://www.radiustheme.com
+License: GPLv2 or later
+Text Domain: gardenar
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
